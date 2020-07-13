f7e5b49dc3bf942ffd2050f7ed4d380987973fba
Francisco Lopez <franlopbri@gmail.com>
2020-07-12 21:47:01 -0500

    crea funcion de consulta

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function conexion($bd_config){
     try {
-        $conexion = new PDO('mysql:host=localhost;dbname=curso_blog', 'root', '');
+        $conexion = new PDO('mysql:host=localhost:3306;dbname=' . $bd_config['basededatos'], $bd_config['usuario'], $bd_config['pass']);
 
         return $conexion;
     } catch (PDOException $e) {
@@ -17,5 +17,18 @@ function limpiarDatos($datos){
 
     return $datos;
 }
+
+function pagina_actual(){
+    return isset($_GET['p']) ? (int)$_GET['p'] : 1;
+}
+
+function obtener_post($post_por_pagina, $conexion){
+    $inicio = (pagina_actual() > 1) ? pagina_actual() * $post_por_pagina - $post_por_pagina : 0;
+    $sentencia = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $post_por_pagina");
+    $sentencia->execute();
+
+    return $sentencia->fetchAll();
+}
+
 
 ?>
