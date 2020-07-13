ac5a9130b3b077d1892c617a724ddffbe248cb31
Francisco Lopez <franlopbri@gmail.com>
2020-07-13 14:48:58 -0500

    queda la pagina single con parametro get

--- views/single.view.php
+++ views/single.view.php
@@ -3,14 +3,14 @@
     <div class="contenedor">
         <div class="post">
             <article>
-                <h2 class="titulo">Título del artículo</h2>
-                <p class="fecha">1 de enero de 2021</p>
+                <h2 class="titulo"><?php echo $post['titulo']?></h2>
+                <p class="fecha"><?php echo fecha($post['fecha']);?></p>
                 <div class="thump">
                     <a href="#">
-                        <img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+                        <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb']?>" alt="">
                     </a>
                 </div>
-                <p class="extracto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio reprehenderit possimus officia, at esse quasi deleniti accusantium. Sint, vitae explicabo autem ex atque earum ipsum ipsam ducimus ratione perspiciatis deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in dolores adipisci eos sint ex amet soluta incidunt a aliquid tempore quasi, quibusdam, provident voluptates blanditiis, dicta corporis nesciunt dolore.</p>
+                <p class="extracto"><?php echo $post['texto']?></p>
             </article>
         </div>
 
